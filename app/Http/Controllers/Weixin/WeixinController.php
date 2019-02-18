51df9d22dd08d0a6c3180e067f4f89a616d913a2
Yue <2429997028@qq.com>
2019-02-18 15:21:40 +0800

    weixin1

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -26,7 +26,10 @@ class WeixinController extends Controller
      */
     public function validToken1()
     {
-        echo 1;
+        //$get = json_encode($_GET);
+        //$str = '>>>>>' . date('Y-m-d H:i:s') .' '. $get . "<<<<<\n";
+        //file_put_contents('logs/weixin.log',$str,FILE_APPEND);
+        echo $_GET['echostr'];
     }
 
     /**
