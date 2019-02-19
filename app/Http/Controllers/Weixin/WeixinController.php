51a71c62f51ba2d12b58443ac361073ed59f908f
Yue <2429997028@qq.com>
2019-02-19 15:57:37 +0800

    weixin20

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -8,21 +8,28 @@ use App\Http\Controllers\Controller;
 
 use Illuminate\Support\Facades\Redis;
 use GuzzleHttp;
+
 class WeixinController extends Controller
 {
     //
 
     protected $redis_weixin_access_token = 'str:weixin_access_token';     //微信 access_token
+
     public function test()
     {
+        //echo __METHOD__;
         echo 'Token: '. $this->getWXAccessToken();
     }
+
     /**
      * 首次接入
      */
     public function validToken1()
     {
-        echo 1;
+        //$get = json_encode($_GET);
+        //$str = '>>>>>' . date('Y-m-d H:i:s') .' '. $get . "<<<<<\n";
+        //file_put_contents('logs/weixin.log',$str,FILE_APPEND);
+        echo $_GET['echostr'];
     }
 
     /**
@@ -32,46 +39,66 @@ class WeixinController extends Controller
     {
         $data = file_get_contents("php://input");
 
+
         //解析XML
         $xml = simplexml_load_string($data);        //将 xml字符串 转换成对象
 
-        $event = $xml->Event;
-        $openid = $xml->FromUserName;                   //事件类型
-        //var_dump($xml);echo '<hr>';
+        $event = $xml->Event;                       //事件类型
+        $openid = $xml->FromUserName;               //用户openid
 
-        if($event=='subscribe') {
-            ;               //用户openid
+
+        // 处理用户发送消息
+        if(isset($xml->MsgType)){
+            if($xml->MsgType=='text'){            //用户发送文本消息
+                $msg = $xml->Content;
+                $xml_response = '<xml><ToUserName><![CDATA['.$openid.']]></ToUserName><FromUserName><![CDATA['.$xml->ToUserName.']]></FromUserName><CreateTime>'.time().'</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA['. $msg. date('Y-m-d H:i:s') .']]></Content></xml>';
+                echo $xml_response;
+                exit();
+            }
+        }
+
+
+        //判断事件类型
+        if($event=='subscribe'){                        //扫码关注事件
+
             $sub_time = $xml->CreateTime;               //扫码关注时间
+
+
+            echo 'openid: '.$openid;echo '</br>';
+            echo '$sub_time: ' . $sub_time;
 
             //获取用户信息
             $user_info = $this->getUserInfo($openid);
+            echo '<pre>';print_r($user_info);echo '</pre>';
 
             //保存用户信息
-            $u = WeixinUser::where(['openid' => $openid])->first();
+            $u = WeixinUser::where(['openid'=>$openid])->first();
             //var_dump($u);die;
-            if ($u) {       //用户不存在
+            if($u){       //用户不存在
                 echo '用户已存在';
-            } else {
+            }else{
                 $user_data = [
-                    'openid' => $openid,
-                    'add_time' => time(),
-                    'nickname' => $user_info['nickname'],
-                    'sex' => $user_info['sex'],
-                    'headimgurl' => $user_info['headimgurl'],
-                    'subscribe_time' => $sub_time,
+                    'openid'            => $openid,
+                    'add_time'          => time(),
+                    'nickname'          => $user_info['nickname'],
+                    'sex'               => $user_info['sex'],
+                    'headimgurl'        => $user_info['headimgurl'],
+                    'subscribe_time'    => $sub_time,
                 ];
+
                 $id = WeixinUser::insertGetId($user_data);      //保存用户信息
+                var_dump($id);
             }
-        }elseif ($event=='CLICK'){     //click  菜单
-            if($xml->EventKey=='kefu01'){
+        }elseif($event=='CLICK'){               //click 菜单
+            if($xml->EventKey=='kefu01'){       // 根据 EventKey判断菜单
                 $this->kefu01($openid,$xml->ToUserName);
             }
-
         }
+
         $log_str = date('Y-m-d H:i:s') . "\n" . $data . "\n<<<<<<<";
         file_put_contents('logs/wx_event.log',$log_str,FILE_APPEND);
-
     }
+
     /**
      * 客服处理
      * @param $openid   用户openid
@@ -83,6 +110,10 @@ class WeixinController extends Controller
         $xml_response = '<xml><ToUserName><![CDATA['.$openid.']]></ToUserName><FromUserName><![CDATA['.$from.']]></FromUserName><CreateTime>'.time().'</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA['. 'Hello World, 现在时间'. date('Y-m-d H:i:s') .']]></Content></xml>';
         echo $xml_response;
     }
+
+
+
+
     /**
      * 接收事件推送
      */
@@ -125,61 +156,66 @@ class WeixinController extends Controller
     public function getUserInfo($openid)
     {
         //$openid = 'oLreB1jAnJFzV_8AGWUZlfuaoQto';
-        $access_token = $this->getWXAccessToken();
+        $access_token = $this->getWXAccessToken();      //请求每一个接口必须有 access_token
         $url = 'https://api.weixin.qq.com/cgi-bin/user/info?access_token='.$access_token.'&openid='.$openid.'&lang=zh_CN';
 
         $data = json_decode(file_get_contents($url),true);
         //echo '<pre>';print_r($data);echo '</pre>';
         return $data;
     }
-    /*
-     *创建服务号菜单
+
+    /**
+     * 创建服务号菜单
      */
     public function createMenu(){
-        //1 获取access_token 拼接请求接口
+        //echo __METHOD__;
+        // 1 获取access_token 拼接请求接口
         $url = 'https://api.weixin.qq.com/cgi-bin/menu/create?access_token='.$this->getWXAccessToken();
+        //echo $url;echo '</br>';
+
         //2 请求微信接口
         $client = new GuzzleHttp\Client(['base_uri' => $url]);
 
         $data = [
             "button"    => [
                 [
-                    "name"=>"秀歌",
-                    "sub_button"=>[
-                        [
-                            "type"  => "click",      // click类型
-                            "name"  => "阴雨天",
-                            "key"   => "kefu01"
-                        ]
-                    ]
+                    "type"  => "view",      // view类型 跳转指定 URL
+                    "name"  => "百度一下",
+                    "url"   => "https://www.baidu.com"
                 ],
                 [
-                    "name"=>"XXX",
-                    "sub_button"=>[
-                        [
-                            "type"  => "view",      // view类型 跳转指定 URL
-                            "name"  => "BBY",
-                            "url"   => "http://yby.52self.cn"
-                        ]
-                    ]
+                    "type"  => "click",      // click类型
+                    "name"  => "客服01",
+                    "key"   => "kefu01"
                 ]
-            ]
+            ],
         ];
 
 
+        $body = json_encode($data,JSON_UNESCAPED_UNICODE);      //处理中文编码
         $r = $client->request('POST', $url, [
-            'body' => json_encode($data,JSON_UNESCAPED_UNICODE)
+            'body' => $body
         ]);
+
         // 3 解析微信接口返回信息
 
         $response_arr = json_decode($r->getBody(),true);
+        //echo '<pre>';print_r($response_arr);echo '</pre>';
+
         if($response_arr['errcode'] == 0){
             echo "菜单创建成功";
         }else{
             echo "菜单创建失败，请重试";echo '</br>';
             echo $response_arr['errmsg'];
+
         }
+
+
+
     }
+
+
+
 
 
 }
