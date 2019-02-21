e9a51017d4cd3d9ca1537d08bfa59f1c1f9ad0e4
Yue <2429997028@qq.com>
2019-02-21 13:59:19 +0800

    admin10

--- app/Admin/Controllers/WeixinMediaController.php
+++ app/Admin/Controllers/WeixinMediaController.php
@@ -91,9 +91,9 @@ class WeixinMediaController extends Controller
         $grid->msg_id('Msg id');
         $grid->local_file_name('Local file name')->display(function($img){
             if(substr($img,-3,3)=='mp4'){
-                $imgs='<a harf="/wx/voice/'.$img.'">观看视频</a>';
+                $imgs='<a href="/wx/voice/'.$img.'">观看视频</a>';
             }elseif(substr($img,-3,3)=='amr'){
-                $imgs='<a harf="/wx/voice/'.$img.'">试听语音</a>';
+                $imgs='<a href="/wx/voice/'.$img.'">试听语音</a>';
             }else{
                 $imgs='<img src="/wx/images/'.$img.'" width=80px;height=80px;> ';
             }
