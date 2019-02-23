750d8d7c1c25b7684c85dab120a9f72c3e6cd8fd
Yue <2429997028@qq.com>
2019-02-23 15:58:08 +0800

    回聊

--- routes/web.php
+++ routes/web.php
@@ -135,3 +135,7 @@ Route::get('/weixin/material/list','Weixin\WeixinController@materialList');     
 Route::get('/weixin/material/upload','Weixin\WeixinController@upMaterial');     //上传永久素材
 Route::post('/weixin/material','Weixin\WeixinController@materialTest');     //创建菜单
 //Route::post('/weixin/material','Weixin\WeixinController@materialTest');     //创建菜单
+
+//微信聊天
+Route::get('/weixin/kefu/chat','Weixin\WeixinController@chatView');     //客服聊天
+Route::get('/weixin/chat/get_msg','Weixin\WeixinController@getChatMsg');     //获取用户聊天信息
