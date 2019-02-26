ca8fd4f1f7eccaa9ae108e6ba1f2186b0629bcbf
Yue <2429997028@qq.com>
2019-02-26 15:09:44 +0800

    支付3

--- app/Http/Controllers/Weixin/ErrocCodeController.php
+++ app/Http/Controllers/Weixin/ErrocCodeController.php
@@ -14,8 +14,8 @@ use App\Http\Controllers\Controller;
 class ErrorCodeController extends Controller
 {
     public static $OK = 0;
-    public static $IllegalAesKey = -41001;//非法
-    public static $IllegalIv = -41002;//
-    public static $IllegalBuffer = -41003;//解密失败
-    public static $DecodeBase64Error = -41004;//解密后得到的buffer非法
+    public static $IllegalAesKey = -41001;
+    public static $IllegalIv = -41002;
+    public static $IllegalBuffer = -41003;
+    public static $DecodeBase64Error = -41004;
 }
